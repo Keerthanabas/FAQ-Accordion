3d504a98e98da99c27a1564ff7975caa28a6a409
Keerthanabas <bkeerthanaeee@gmail.com>
2023-11-07 18:29:02 +0530

    adding css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      FAQ
+    <h1>Project 2: FAQ/Accordion</h1>
+      <h2 className='box'>Frequently Asked Questions</h2>
     </div>
   );
 }
