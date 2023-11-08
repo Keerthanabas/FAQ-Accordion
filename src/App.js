3c45fbe12b9d1c6d11d8a328074fafbd4a4fcc44
Keerthanabas <bkeerthanaeee@gmail.com>
2023-11-08 17:50:07 +0530

    staring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+import FAQ from "./faq";
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-    <h1>Project 2: FAQ/Accordion</h1>
-      <h2 className='box'>Frequently Asked Questions</h2>
+    < FAQ/>
     </div>
   );
 }
--- src/faq.js
+++ src/faq.js
@@ -1,2 +1,14 @@
 import React from "react";
+import data from "./data";
+
+
+const FAQ = () =>{
+return (
+<div>
+    hello
+</div>
+
+)
+}
+export default FAQ
 
