6727bae9618657fdcbf8cc8409908e5f8e37f8ea
Keerthanabas <bkeerthanaeee@gmail.com>
2023-11-08 17:44:19 +0530

    adding data

--- src/data.js
+++ src/data.js
@@ -1,6 +1,23 @@
-export const data =[{
-    
+export const data =[
+    {
+    question :"What is Reactjs?",
+    answer : "React.js is a JavaScript library for building user interfaces."
+    },
+    {
+        question :"what is reactjs purpose?",
+        answer : "The purpose of React.js is to build user interfaces for web applications with a focus on creating reusable and efficient UI components."
+    },
+    {
+        question :"what is reactjs advantage?",
+        answer : "Efficient and reusable user interface components for building interactive web applications."
+    },
+    {
+        question :"what is reactjs disadvantage?",
+        answer : "ReactJS can have a steeper learning curve for beginners."
+    },
+    {
+        question :"What are the Components lifecycle in reactjs? ",
+        answer : "React components go through a lifecycle that includes mounting, updating, and unmounting phases. You can hook into these lifecycle events to perform actions at specific points in a component's existence."
+    }
 
-    
-
-}]
+    ]
