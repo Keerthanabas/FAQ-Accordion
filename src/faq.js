c42e66d276db08dcbb959401f814bc3d16a68790
Keerthanabas <bkeerthanaeee@gmail.com>
2023-11-09 12:38:37 +0530

    9.11.23 adding

--- src/faq.js
+++ src/faq.js
@@ -4,19 +4,24 @@ import {data} from "./data";
 
 const FAQ = () =>{
 return (
-<div>
+<div >
     <h1>Project 2 : FAQ/Accordian </h1>
     <h2>Frequently Asked Questions</h2>
+    <div className="box">
+        <div className="accordion">
     {data.map((item,index)=>{
         return(
-            <div className="box">
-                <wrap>
+            <div >
+                <wrap >
                <h3>{item.question}</h3>
                <p>{item.answer}</p>
                </wrap>
             </div>
+            
         )
     })}
+    </div>
+    </div>
 </div>
 
 )
