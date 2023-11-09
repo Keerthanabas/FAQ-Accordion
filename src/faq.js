eff074892d95f2f9477824b6432182bf4b86118c
Keerthanabas <bkeerthanaeee@gmail.com>
2023-11-09 13:17:08 +0530

    over

--- src/faq.js
+++ src/faq.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import {data} from "./data";
 
 
 const FAQ = () =>{
+    const[selected,setSelected] = useState(null)
+    const toggle =(index)=>{
+        if(selected === index){ 
+            return setSelected(null)
+         }
+         setSelected(index)
+    }
+
+    
 return (
 <div >
     <h1>Project 2 : FAQ/Accordian </h1>
@@ -13,11 +22,11 @@ return (
         return(
             <div className="item">
                 <wrap>
-                    <div className="title">
+                    <div className="title" onClick={()=>toggle(index)}>
                <h3 >{item.question}</h3>
-               <span>+</span>
+               <span>{selected  === index ? '-' : '+'}</span>
                </div>
-               <p className="content">{item.answer}</p>
+               <p className={selected === index ? 'content show' : 'content'}>{item.answer}</p>
                </wrap>
             </div>
             
